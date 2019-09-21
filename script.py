8454ef8e32a72ae2b514fa448b4a0c084b703c21
Reema Mohsin <reemamohsin4@gmail.com>
2019-09-21 15:34:58 -0700

    Added comments

--- script.py
+++ script.py
@@ -1,11 +1,17 @@
 import codecademylib
 import pandas as pd
 
+#load data
 inventory = pd.read_csv('inventory.csv')
+
+#get first 10 items of inventory
 staten_island = inventory.loc[:10]
 product_request = staten_island['product_description']
 
+#find Brooklyn seeds
 seed_request = inventory[(inventory['location']=='Brooklyn')&(inventory['product_type']=='seeds')]
+
+#calculates total value of all items in stock
 mylambda = lambda x: True if x>0 else False
 inventory['in_stock'] = [mylambda(q) for q in inventory['quantity']]
 inventory['total_value'] = inventory.price*inventory.quantity
